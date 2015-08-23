9e701264b51e90886e239ca12ebeb38a4a1a41ba
Abdo Roig-Maranges <abdo.roig@gmail.com>
2015-08-23 12:37:22 +0200

    update mkpreamble

--- paper/{{cookiecutter.project_name}}/bin/mkpreamble.py
+++ paper/{{cookiecutter.project_name}}/bin/mkpreamble.py
@@ -6,6 +6,7 @@
 import sys
 import argparse
 import json
+import re
 
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
@@ -18,53 +19,93 @@ def load_bibtex(f):
     parser.alt_dict = {}
 
     with open(f, 'r') as fd:
-        bib = bibtexparser.loads(fd.read(), parser=parser)
+        txt = re.sub('^\s*%.*$', '', fd.read(), flags=re.MULTILINE)
+        bib = bibtexparser.loads(txt, parser=parser)
 
     return bib.entries
 
 
-def print_entry(pattern, *values):
-    if all(values):
-        print(pattern % values)
+def getstr(d, key, default=None):
+    val = d.get(key, None)
+    if val != None and len(val.strip()) > 0: return val.strip()
+    else:                                    return default
 
 
-def print_abstract(metadata):
-    abstract = metadata.get('abstract',None)
-    print('')
-    print(r'\newcommand{\makeabstract}{')
-    if abstract != None and len(abstract.strip()) > 0:
-        print_entry('\\begin{abstract}\n%s\n\\end{abstract}', abstract)
-    print(r'}')
+def getlist(d, key, sep=',\s*'):
+    val = getstr(d, key)
+    if val: return [a.strip() for a in re.split(sep, val)]
+    else:   return []
 
 
-def print_authorlist(authors):
-    print("")
-    print(r'\newcommand{\makeauthorlist}{')
+def format_entry(pattern, *values):
+    if all(values): return pattern % values
+    else:           return ''
+
+
+def format_command(name, content):
+    return '\\newcommand{\\%s}{\n%s\n}' % (name, content)
+
+
+def format_title(metadata):
+    title = getstr(metadata, 'title', '')
+    thanks = getstr(metadata, 'thanks')
+    mrclass = getlist(metadata, 'mrclass')
+    # TODO: keywords?
+
+    L = [title]
+
+    if thanks:
+        L.append(r'\thanks{%s}' % thanks)
+
+    if mrclass:
+        L.append(r'  \unfootnote{\textbf{AMS CLassification:} %s.}' % (', '.join(mrclass)))
+
+    return '\n'.join(L)
+
+
+def format_authors(authors):
+    # TODO: individual thanks
+    return r' \and '.join([a['name'] for a in authors])
+
+
+def format_date(metadata):
+    date = metadata.get('date', None)
+    if date:
+        y, m, d = date.split('-')
+        return r'\formatdate{%s}{%s}{%s}' % (d.strip(), m.strip(), y.strip())
+
+
+def format_abstract(metadata):
+    return metadata.get('abstract', '')
+
+
+def format_authorlist(authors):
+    L = []
     for a in authors:
-        print(r'  \vspace{1em}')
-        print(r'  \parbox[t]{0.5\textwidth}{')
-        print_entry(r'    \textbf{%s}\\',       a.get('name',None))
-        print_entry(r'    \texttt{%s}\\',       a.get('email',None))
-        print_entry(r'    \texttt{\url{%s}}\\', a.get('web',None))
-        print_entry(r'    %s\\',                a.get('university',None))
-        print_entry(r'    %s\\',                a.get('department',None))
-        print_entry(r'    %s\\',                a.get('address',None))
-        print_entry(r'    %s\\',                a.get('city',None))
-        print(r'  }')
-    print(r'}')
+        L.append(r'  \vspace{1em}')
+        L.append(r'  \parbox[t]{0.5\textwidth}{')
+        L.append(format_entry(r'    \textbf{%s}\\',       a.get('name',None)))
+        L.append(format_entry(r'    \texttt{%s}\\',       a.get('email',None)))
+        L.append(format_entry(r'    \texttt{\url{%s}}\\', a.get('web',None)))
+        L.append(r'    \\')
+        L.append(format_entry(r'    %s\\',                a.get('university',None)))
+        L.append(format_entry(r'    %s\\',                a.get('department',None)))
+        L.append(format_entry(r'    %s\\',                a.get('address',None)))
+        L.append(format_entry(r'    %s\\',                a.get('city',None)))
+        L.append('  }')
+
+    return '\n'.join(L)
 
 
-def print_preamble(metadata, authors):
-    print('% Auto-generated preamble')
-    print('')
-    print_entry(r'\title{%s}',          metadata.get('title',None))
-    print_entry(r'\author{%s}',         metadata.get('author',None))
-    print_entry(r'\date{%s}',           metadata.get('date',None))
-    print_abstract(metadata)
-    print_authorlist(authors)
-
-
-
+def format_preamble(metadata, authors):
+    L = []
+    L.append('% Auto-generated preamble')
+    L.append(format_entry(r'\title{%s}', format_title(metadata)))
+    L.append(format_entry(r'\author{%s}', format_authors(authors)))
+    L.append(format_entry(r'\date{%s}', format_date(metadata)))
+    L.append(format_command('makeabstract', format_abstract(metadata)))
+    L.append(format_command('makeauthorlist', format_authorlist(authors)))
+    return '\n\n'.join(L)
 
 
 # ----------------------------------------------------------------------- #
@@ -89,4 +130,4 @@ for e in entries:
     elif e['ENTRYTYPE'] == 'misc':
         authors.append(e)
 
-print_preamble(metadata, authors)
+print(format_preamble(metadata, authors))
